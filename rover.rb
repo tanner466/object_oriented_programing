2d9dbfa13f068f6921670f28680545cddf94a555
tanner466 <tanner466@gmail.com>
2016-01-14 14:26:43 -0500

    It's working but sorta ugly

--- rover.rb
+++ rover.rb
@@ -1,29 +1,90 @@
 class Rover
   attr_reader :x_coord, :y_coord, :direction
   def initialize(x_coord , y_coord , direction)
-    @x_coord = x_coord
-    @y_coord = y_coord
+    @x_coord = x_coord.to_i
+    @y_coord = y_coord.to_i
     @direction = direction
+    puts "New rover #{@x_coord} #{@y_coord} #{@direction}"
   end
 
   def read_instruction(instruction)
     instruction.each_char  do |char|
-      if char.downcase=="m"
+      if char.upcase=="M"
         move
       else
-        turn
+        turn(char.upcase)
       end
     end
+
+    puts "#{x_coord} #{y_coord} #{direction}"
   end
 
   def move
-    puts "move"
+    if @direction == "N"
+      @y_coord += 1
+    elsif @direction == "E"
+      @x_coord += 1
+    elsif @direction == "S"
+      @y_coord -= 1
+    else
+      @x_coord -= 1
+    end
   end
 
-  def turn
-    puts "turn"
+  def turn(direction_turn)
+    if direction_turn == "R"
+      if @direction == "N"
+        @direction = "E"
+      elsif @direction == "E"
+        @direction = "S"
+      elsif @direction == "S"
+        @direction = "W"
+      else
+        @direction = "N"
+      end
+
+
+    else
+      if @direction == "N"
+        @direction = "W"
+      elsif @direction == "W"
+        @direction = "S"
+      elsif @direction == "S"
+        @direction = "E"
+      else
+        @direction = "N"
+      end
+
+    end
   end
 end
 
-my_rover = Rover.new(0,0,"N")
-my_rover.read_instruction("MMRMMRMRRM")
+class Plateau
+  def initialize(x_length , y_length)
+    @grid = Array.new(x_length){Array.new(x_length)}
+  end
+end
+
+
+print "What's the plateau size: "
+plateau_string = gets.chomp
+
+print "First rover initial position and direction facing: "
+rover_1_position = gets.chomp
+
+print "First rover move sequence: "
+rover_1_instructions = gets.chomp
+
+print "Second rover initial position and direction facing: "
+rover_2_position = gets.chomp
+
+print "Second rover move sequence: "
+rover_2_instructions = gets.chomp
+
+
+puts "Output"
+rover_1 = Rover.new(rover_1_position.chars[0],rover_1_position.chars[2],rover_1_position.chars[4])
+rover_1.read_instruction(rover_1_instructions)
+rover_2 = Rover.new(rover_2_position.chars[0],rover_2_position.chars[2],rover_2_position.chars[4])
+rover_2
+rover_2.read_instruction(rover_2_instructions)
